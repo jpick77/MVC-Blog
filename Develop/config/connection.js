44ec6ec76f2eb14d1164ec1fcfc7c1a74cedc613
Jacob Pickron <jpickron30@gmail.com>
2023-04-05 18:58:50 -0600

    ....

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-
+require('dotenv').config();
 
 let sequelize;
 
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql'
     }
   );
